6f90a873cc69d44f564419b1f56e799cee3efebb
teostojkovski <teo.stojkovski@gmail.com>
2025-02-26 16:12:07 +0100

    Image change

--- frontend/src/components/Home/Home.jsx
+++ frontend/src/components/Home/Home.jsx
@@ -10,7 +10,7 @@ const Home = () => {
         <section className='banner -z-50 relative flex flex-col justify-center items-center'>
             <div className='w-[50vw] z-10 text-center'>
                 <p className='text-2xl lg:text-6xl font-bold z-10 py-5'>Welcome to Enigma</p>
-                <p className='z-10 text-gray-300 text-xl'>Where Culinary Art Meets Mystery—Indulge in an Extraordinary Dining Experience</p>
+                <p className='z-10 text-gray-300 text-xl'>Where Culinary Art Meets Mystery. Indulge in an Extraordinary Dining Experience</p>
             </div>
             <div className='cover absolute top-0 left 0 right-0'
             ></div>
